22c450263ed32e49428ba97f81c74d09e76a12f0
Azlaan Zubair <azlaanzubair4@gmail.com>
2019-05-16 23:32:52 +0500

    Chapter 4,6 & 7 --  Familiar operators, Unfamiliar operators and Eliminating ambiguity

---
+++ Chapters/Math Operators.py
@@ -0,0 +1,42 @@
+print("\nAddition")
+add = "4+2 = " + str(4+2)
+print(add)
+
+print("\nSubtraction")
+subtract = "4-2 = " + str(4-2)
+print(subtract)
+
+print("\nMultiplication")
+multiply = "4*2 = " + str(4*2)
+print(multiply)
+
+print("\nDivision")
+division = "4/2 = " + str(4/2)
+print(division)
+
+print("\nPercentage")
+percentage = "(2/4)*100 = " + str((4/2)*100)
+print(percentage)
+
+print("\nModulus")
+modulus = "4%2 = " + str(7/2)
+print(modulus)
+
+print("\nAverage")
+average = "(5+6+9+3+4+7)/6 = " + str((5+6+9+3+4+7)/6)
+print(average)
+
+print("\nIncrement 5")
+ageup = 50
+ageup += 5
+print(ageup)
+
+print("\nDecrement 5")
+agedown = 50
+agedown -= 5
+print(agedown)
+
+print("\nIncrement 5 Times")
+agetimes = 50
+agetimes *= 5
+print(agetimes)
