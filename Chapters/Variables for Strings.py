4ffa8ee0dcb6971b1f64f7aa501f1682b7a6469a
Azlaan Zubair <azlaanzubair4@gmail.com>
2019-05-16 20:54:49 +0500

    Chapter 2 and 3 -- Variables for Strings and Number

---
+++ Chapters/Variables for Strings.py
@@ -0,0 +1,19 @@
+#variables
+name = "Muhammad Azlaan Zubair"
+email = "azlaanzubair4@gmail.com"
+program = "Artificial Intelligence"
+location = "Sindh Boys Scout"
+head = "PIAIC"
+roll = "AIC017585"
+batch = 1
+city = "Karachi"
+
+print("\n \nHi there, myself " + name + ", student of " + head + ". Enrolled in program " + program + ", at " + location + ".")
+
+print("\n==================")
+print("Basic Info. :")
+print("Email : " + email)
+print("Roll No. : " + roll)
+print("Batch : Batch", batch)
+print("City : " + city)
+print("==================\n")
