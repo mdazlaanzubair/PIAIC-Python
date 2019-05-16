428dfed8896dd7d014f3388a47ebe741e17bf48f
Azlaan Zubair <azlaanzubair4@gmail.com>
2019-05-16 20:54:03 +0500

    Chapter 1 -- Print

---
+++ Chapters/Print.py
@@ -0,0 +1,5 @@
+print ("hello world") # simple print
+print ("hello world" + " " + "universe") # string concatenation
+print ("2+2") # string numbers to string
+print (2) # integer print
+print (2+2) # adding integers
